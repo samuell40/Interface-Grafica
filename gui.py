3b2b9ee09c416d8b4ab36784ca18916aabf7f49d
samuel <samuelpc4567@gmail.com>
2024-04-23 21:16:51 -0300

    Estilização da Interface
    Aqui foi feita a aparência visual da interface gráfica, incluindo cores, fontes, tamanhos e tamanho das janelas, textos e botões.

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 from guizero import App, Window, Text, TextBox, PushButton, Box
+from classe import verificar_codigo, verificar_usuario
+from persistencia import autenticacao, cadastrar_usuario, buscar_usuario, excluir_usuario
 
 
 def testar_senha(event_data):
@@ -79,4 +81,129 @@ def excluir():
 
 def fechar_app():
     app.destroy()
+
+
+
+# login
+app = App(title='Login', height=130, width=300, bg='#dcdcdc')
+
+
+bottom_box = Box(app, align='bottom', layout='grid')
+top_box = Box(app, align='bottom', layout='grid')
+
+
+Text(app, text='LOGIN', font='Georgia', grid=[1, 0], size=18, color='#606060')
+
+
+Text(top_box, font='Georgia', text='Usuário: ', grid=[0, 1])
+usuario = TextBox(top_box, width=30, grid=[1, 1])
+Text(top_box, font='Georgia', text='Senha:', grid=[0, 2])
+senha = TextBox(top_box, width=30, hide_text=True, grid=[1, 2])
+senha.when_key_pressed = testar_senha
+
+
+usuario.tk.config(bg='white')
+senha.tk.config(bg='white')
+
+
+enviar = PushButton(bottom_box, text='Enviar', command=autenticar, width=16, grid=[0, 0])
+cadastro_btn = PushButton(bottom_box, text='Novo Usuário', command=abrir_cad, width=16, grid=[1, 0])
+
+
+enviar.tk.config(bg='#b0b0b0')
+cadastro_btn.tk.config(bg='#b0b0b0')
+
+
+# principal
+principal = Window(app, title='Janela Principal', width=350, height=210, bg='#dcdcdc')
+box = Box(principal, align='bottom', layout='grid')
+Text(principal, font='Georgia', text='JANELA PRINCIPAL', size='18', color='#606060', grid=[0, 0])
+
+
+cadastro_btn2 = PushButton(box, image="./Imagens/adc_usuario.png", command=abrir_cad, width=100, height=55, grid=[0, 0])
+Text(box, font='Georgia', text='Cadastrar Usuário', grid=[0, 1])
+
+
+busca_btn = PushButton(box, image="./Imagens/lupa.png", command=abrir_busca, width=100, height=55, grid=[1, 0])
+Text(box, font='Georgia', text='Buscar Usuário', grid=[1, 1])
+
+
+excluir_btn = PushButton(box, image="./Imagens/lixeira.png", command=abrir_exc, width=100, height=55, grid=[0, 2])
+Text(box, font='Georgia', text='Excluir Usuário', grid=[0, 3])
+
+
+cancelar = PushButton(box, image="./Imagens/x.png", command=fechar_app, width=100, height=55, grid=[1, 2])
+Text(box, font='Georgia', text='Encerrar Aplicação', grid=[1, 3])
+
+
+cadastro_btn2.tk.config(bg='#b0b0b0')
+busca_btn.tk.config(bg='#b0b0b0')
+excluir_btn.tk.config(bg='#b0b0b0')
+cancelar.tk.config(bg='#e74c3c')
+
+
+principal.hide()
+principal.when_closed = fechar_app
+
+
+# cadastro
+cadastro = Window(app, title='Cadastrar', bg='#dcdcdc', height=125, width=240)
+cad_box = Box(cadastro, align='top', layout='grid')
+
+
+Text(cad_box, font='Georgia', text='Código: ', grid=[0, 0])
+codigo_cad = TextBox(cad_box, width=20, grid=[1, 0])
+Text(cad_box, font='Georgia', text='Usuário: ', grid=[0, 1])
+usuario_cad = TextBox(cad_box, width=20, grid=[1, 1])
+Text(cad_box, font='Georgia', text='Senha: ', grid=[0, 2])
+senha_cad = TextBox(cad_box, width=20, hide_text=True, grid=[1, 2])
+cadastrar = PushButton(cadastro, text='Cadastrar', command=cadastrar, width=15)
+
+
+cadastrar.tk.config(bg='#b0b0b0')
+codigo_cad.tk.config(bg='white')
+usuario_cad.tk.config(bg='white')
+senha_cad.tk.config(bg='white')
+cadastro.hide()
+
+
+# buscar
+busca = Window(app, title='Buscar', height=75, width=240, bg='#dcdcdc')
+busca_box = Box(busca, align='top', layout='grid')
+
+
+Text(busca_box, font='Georgia', text='Usuário: ', grid=[0, 0])
+usuario_busc = TextBox(busca_box, width=20, grid=[1, 0])
+buscar = PushButton(busca, text='Buscar', command=buscar, width=15)
+
+
+buscar.tk.config(bg='#b0b0b0')
+usuario_busc.tk.config(bg='white')
+busca.hide()
+
+
+# excluir
+exclusao = Window(app, title='Excluir', height=75, width=240, bg='#dcdcdc')
+exc_box = Box(exclusao, align='top', layout='grid')
+
+
+Text(exc_box, font='Georgia', text='Código: ', grid=[0, 0])
+codigo_exc = TextBox(exc_box, width=20, grid=[1, 0])
+excluir = PushButton(exclusao, text='Excluir', command=excluir, width=15)
+
+
+excluir.tk.config(bg='#b0b0b0')
+codigo_exc.tk.config(bg='white')
+exclusao.hide()
+
+
+# display do app e proibição de redimensionamento
+app.tk.resizable(False, False)
+principal.tk.resizable(False, False)
+cadastro.tk.resizable(False, False)
+busca.tk.resizable(False, False)
+exclusao.tk.resizable(False, False)
+app.display()
+
+
 
